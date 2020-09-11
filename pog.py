d48a523f4a9f28d076d757f1ede9e083d25da9a3
vlad karachev <vladkarachov@gmail.com>
2020-09-11 22:19:52 +0300

    fixes for pog func - now stats includes only successful pogs

--- pog.py
+++ pog.py
@@ -26,7 +26,8 @@ def pog(update: Updater, context):
             last_time + datetime.timedelta(minutes=1) > update.effective_message.date:
         update.message.reply_text('не погай')
     else:
-        update_poggers_stats(update.effective_user.name, update.effective_chat.id, poggers_chats)
+        if sticker == stickers[1]:
+            update_poggers_stats(update.effective_user.name, update.effective_chat.id, poggers_chats)
         context.bot.sendSticker(update.effective_chat.id,
                                 sticker=sticker)
         pog_users_time[update.effective_user.id] = update.effective_message.date
@@ -34,7 +35,7 @@ def pog(update: Updater, context):
 
 def update_poggers_stats(userid, chatid, poggers_chats):
     # шоб людей не тегало
-    userid=userid[1:]
+    userid = userid[1:]
     poggers_chat = poggers_chats.get(chatid, 0)
     if not poggers_chat:
         poggers_chat = {userid: 1}
@@ -51,10 +52,15 @@ def get_pog_stats(update: Updater, context):
     poggers_list = ''
     i = 0
     poggers = poggers_chats.get(update.effective_chat.id, 0)
-    if type(poggers)==type(dict()):
+    if type(poggers) == type(dict()):
         for pogger in sorted(poggers, key=poggers.get, reverse=True):
             poggers_list += '\n' + str(pogger) + ":" + str(poggers[pogger])
-        update.message.reply_text("curren POG prob : "
+            if i < 5:
+                i += 1
+            else:
+                break
+    if poggers_list!='':
+        poggers_list='\nTop 5 poggers:'+ poggers_list
+    update.message.reply_text("curren POG prob : "
                                   + str(float('{:.2f}'.format(current_lunar_phase[1])))
-                                  + '\nTop 5 poggers :'
                                   + poggers_list)
